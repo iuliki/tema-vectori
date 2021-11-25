42e235f281e7ddeb05d4848f29bde54dd945ac53
iuliki <iulia.bulugu@emanuel.ro>
2021-11-25 15:32:50 +0200

    rezolvare citire

---
+++ TemaVectori/TemaVectori/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TemaVectori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Print(int[] array)
+        {
+            if( array is null)
+            {
+                Console.WriteLine("array is null");
+                return;
+            }
+            Console.Write("array= ");
+            for(int i=0;i<array.Length;i++)
+            {
+                int element = array[i];
+                Console.Write(alemenet);
+                if(i<array.length -1)
+                {
+                    Console.Write(",");
+                }
+            }
+        }
+    }
+}
